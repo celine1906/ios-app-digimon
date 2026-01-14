6e9b594d82c932db985515e46b0ecd6371c3f663
celine1906 <reginaceline1906@gmail.com>
2026-01-14 14:57:45 +0700

    Seperate DigimonCardModel

--- Digimon/View/DigimonCardComponent.swift
+++ Digimon/View/DigimonCardComponent.swift
@@ -178,45 +178,6 @@ final class DigimonLoadingCell: UITableViewCell {
     }
 }
 
-struct DigimonCardViewModel {
-    let name: String
-    let imageUrl: String?
-    let metaInfo: String?
-    let fields: [FieldViewModel]
-    
-    struct FieldViewModel {
-        let name: String
-        let iconUrl: String?
-    }
-    
-    init(from model: DigimonModel) {
-        self.name = model.name
-        self.imageUrl = model.image
-        
-        guard let detail = model.cachedDetail else {
-            self.metaInfo = nil
-            self.fields = []
-            return
-        }
-        
-        var metaParts: [String] = []
-        if let level = detail.levels?.first?.level {
-            metaParts.append(level)
-        }
-        if let type = detail.types?.first?.type {
-            metaParts.append(type)
-        }
-        if let attribute = detail.attributes?.first?.attribute {
-            metaParts.append(attribute)
-        }
-        self.metaInfo = metaParts.isEmpty ? nil : metaParts.joined(separator: " | ")
-        
-        self.fields = detail.fields?.map { field in
-            FieldViewModel(name: field.field, iconUrl: field.image)
-        } ?? []
-    }
-}
-
 final class DigimonCell: UITableViewCell {
     private let cardView = DigimonCardView()
 
